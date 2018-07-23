3557d407344a05061d00e10fe23abefe269638ea
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-23 20:36:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,9 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    binding.pry
     students = doc.css(".roster-cards-container")
+    students.each do |student|
+    end
   end
 
   def self.scrape_profile_page(profile_url)
