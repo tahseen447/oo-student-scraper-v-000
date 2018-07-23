1170bd81aa4a765d38e9bb7465e74df46fb2c48e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-23 20:33:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open-uri(index_url))
+    doc = Nokogiri::HTML(open(index_url))
   end
 
   def self.scrape_profile_page(profile_url)
